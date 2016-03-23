bbc22c3c647dacab76d3fcadcd7693d744be2144
Timur Batyrshin <erthad@gmail.com>
2016-03-23 17:54:45 +0300

    rename scripts to start sender, worker and ui; cleanup test data on startup of these

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+rm -f checkins.txt
 bundle exec ruby -Ilib "$(dirname $0)/lib/sender.rb"
--- worker.sh
+++ worker.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+rm -f checkins.txt
 bundle exec sidekiq -r "$(dirname $0)/lib/myworker.rb"
