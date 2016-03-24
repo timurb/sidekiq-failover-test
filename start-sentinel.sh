345851cfbbd6f20feb0855a3bc20a2df32702379
Timur Batyrshin <erthad@gmail.com>
2016-03-24 17:35:47 +0300

    update sentinel config and startup scripts

--- start-sentinel.sh
+++ start-sentinel.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 for N in 1 2 3; do
-  cp -f etc/redis-sentinel.conf /tmp/redis-sentinel$N.conf
-  sed -i "s,XXX,$N,g" /tmp/redis-sentinel$N.conf
-  chmod a+w /tmp/redis-sentinel$N.conf
+  sudo cp -f etc/redis-sentinel.conf /tmp/redis-sentinel$N.conf
+  sudo sed -i "s,XXX,$N,g" /tmp/redis-sentinel$N.conf
+  sudo chown redis /tmp/redis-sentinel$N.conf
   sudo -u redis redis-sentinel /tmp/redis-sentinel$N.conf &
 done
